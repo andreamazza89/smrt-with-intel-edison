46dfd3fbc3bfb36b9f1c97ec450181d73dea9679
kpuwal <kpuwal@gmail.com>
2016-07-26 16:42:26 +0100

    added weather api setup

--- app/server.js
+++ app/server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('./app/public'));
+app.use('/widgets', express.static('./app/widgets'));
 
 app.get('/mirror', function(req, res){
   res.sendfile('./app/public/mirror.html');
 });
+
 
 app.listen(3000, function(){
   console.log('listening at http://localhost:3000');
