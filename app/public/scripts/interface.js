03ae1042d55f937b650b7a2483d81fd6981cc212
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-07-28 11:01:16 +0100

    ajax added

--- app/public/scripts/interface.js
+++ app/public/scripts/interface.js
@@ -1,45 +1,44 @@
 $(document).ready(function(){
 
-  var wjson = new WidgetJSON();
+  function logger(data) {
+    console.log(data);
+  }
 
   $('.widget-check').click(function(event) {
     var widgetName = $(this).attr('id').match(/\w+$/)[0];
-    //
-    // toggleActive(widgetName);
-    // setListStyles();
+    getJSON(toggleActive, widgetName);
+    getJSON2(setListStyles);
     // setDragBox(widgetName);
-
-    wjson.toggleActive(widgetName);
   });
-  //
-  // var toggleActive = function(widgetName) {
-  //   var widgets = getJSON();
-  //   for(var i = 0; i < widgets.length; i++) {
-  //     if (widgets[i].name === widgetName) {
-  //       widgets[i].active === "true" ? widgets[i].active = "false" :  widgets[i].active = "true";
-  //       writeJSON(widgets);
-  //       return widgets[i].active;
-  //     }
-  //   }
-  // };
-  //
-  // var setListStyles = function()  {
-  //   var widgets = getJSON();
-  //   for (var i = 0; i < widgets.length; i++) {
-  //     var name = widgets[i].name;
-  //     if(widgets[i].active === "true") {
-  //       $("#widget-name-"+name).attr('class', 'widget-name-' + name +'-active');
-  //       $("#widget-check-"+name).attr('class', 'widget-check-' + name +'-active');
-  //     }
-  //     else {
-  //       $("#widget-name-"+name).attr('class', 'widget-name');
-  //       $("#widget-check-"+name).attr('class', 'widget-check');
-  //     }
-  //   }
-  // }
-  //
-  // var setDragBox(widgetName)  {
-  //   $("#some-element").html().append("<div id='widget-box-"+widgetName+"'></div>");
-  // }
+
+  var toggleActive = function(data, widgetName) {
+    var widgets = JSON.parse(data).widgets;
+    for(var i = 0; i < widgets.length; i++) {
+      if (widgets[i].name === widgetName) {
+        widgets[i].active === "true" ? widgets[i].active = "false" :  widgets[i].active = "true";
+        setJSON({ widgets: widgets });
+        return widgets[i].active;
+      }
+    }
+  };
+
+  var setListStyles = function(data)  {
+    var widgets = JSON.parse(data).widgets;
+    for (var i = 0; i < widgets.length; i++) {
+      var name = widgets[i].name;
+      if(widgets[i].active === "true") {
+        $("#widget-name-"+name).attr('class', 'widget-name-' + name +'-active');
+        $("#widget-check-"+name).attr('class', 'widget-check-' + name +'-active');
+      }
+      else {
+        $("#widget-name-"+name).attr('class', 'widget-name');
+        $("#widget-check-"+name).attr('class', 'widget-check');
+      }
+    }
+  }
+
+  var setDragBox = function(widgetName)  {
+    $("#some-element").html().append("<div id='widget-box-"+widgetName+"'></div>");
+  }
 
 });
