396692d8fdbb974da9934d22a1a1c22d4199cbbf
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-07-28 14:22:20 +0100

    button styling updated

--- app/public/scripts/interface.js
+++ app/public/scripts/interface.js
@@ -2,31 +2,16 @@ $(document).ready(function(){
   var widgetJSON = require('/framework.js').widgetJSON;
   var wjson = new widgetJSON();
 
-  function logger(data) {
-    console.log(data);
-  }
+  getJSON(setListStyles);
 
   $('.widget-check').click(function(event) {
     var widgetName = $(this).attr('id').match(/\w+$/)[0];
+    toggleStyles(widgetName);
     getJSON(wjson.toggleActive, widgetName);
-    // getJSON2(setListStyles);
+    // getJSON(setListStyles, widgetName);
     // setDragBox(widgetName);
   });
 
-  var setListStyles = function(data)  {
-    var widgets = JSON.parse(data).widgets;
-    for (var i = 0; i < widgets.length; i++) {
-      var name = widgets[i].name;
-      if(widgets[i].active === "true") {
-        $("#widget-name-"+name).attr('class', 'widget-name-' + name +'-active');
-        $("#widget-check-"+name).attr('class', 'widget-check-' + name +'-active');
-      }
-      else {
-        $("#widget-name-"+name).attr('class', 'widget-name');
-        $("#widget-check-"+name).attr('class', 'widget-check');
-      }
-    }
-  };
 
   var setDragBox = function(widgetName)  {
     $("#some-element").html().append("<div id='widget-box-"+widgetName+"'></div>");
---
+++ app/public/scripts/styling.js
@@ -0,0 +1,27 @@
+var setListStyles = function(data, widgetName)  {
+  var widgets = JSON.parse(data).widgets;
+  for (var i = 0; i < widgets.length; i++) {
+    var name = widgets[i].name;
+    if(widgets[i].active === "true") {
+      $("#widget-name-"+name).attr('class', 'widget-name-' + name +'-active');
+      $("#widget-check-"+name).attr('class', 'widget-check-' + name +'-active');
+    }
+    else {
+      $("#widget-name-"+name).attr('class', 'widget-name');
+      $("#widget-check-"+name).attr('class', 'widget-check');
+    }
+  }
+};
+
+var toggleStyles = function(name)  {
+    console.log(name);
+    if($("#widget-name-"+name).attr('class') === 'widget-name') {
+      $("#widget-name-"+name).attr('class', 'widget-name-' + name +'-active');
+      $("#widget-check-"+name).attr('class', 'widget-check-' + name +'-active');
+    }
+    else {
+      $("#widget-name-"+name).attr('class', 'widget-name');
+      $("#widget-check-"+name).attr('class', 'widget-check');
+    }
+
+};
