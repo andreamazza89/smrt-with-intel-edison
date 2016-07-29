5882c7111919a4f1bbd4793e1ab06da37c5e58fe
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-07-29 16:21:46 +0100

    code refactored

--- app/public/scripts/interface.js
+++ app/public/scripts/interface.js
@@ -1,23 +1,25 @@
 var getJSON = require('./json').getJSON;
 var setJSON = require('./json').setJSON;
 var widgetJSON = require('./framework').widgetJSON;
-var toggleStyles = require('./styling').toggleStyles;
-var setListStyles = require('./styling').setListStyles;
 
 var positionKey = { 0: 0, 205: 1, 410: 2 };
 var positionKey2 = { 0: 0, 1: 205, 2: 410 };
+
+var draggableConfig = { snap: ".grid-box",
+                        snapMode: "inner",
+                        snapTolerance: 103,
+                        containment: "#grid-container",
+                        cursor: "pointer" };
 
 $(document).ready(function(){
 
   var wjson = new widgetJSON(setJSON);
 
-  getJSON(setListStyles);
-
   $('.widget-check').click(function(event) {
     var widgetName = $(this).data('name');
-    toggleStyles(this);
+    $(this).parent().toggleClass('active');
     getJSON(wjson.toggleActive, widgetName);
-    setDragBox(widgetName);
+    setDragBox(this);
   });
 
   $('body').on('mouseup', '.widget-box', function(event){
@@ -26,12 +28,15 @@ $(document).ready(function(){
     getJSON(wjson.setGridPosition, widgetName, location);
   });
 
-  var setDragBox = function(widgetName)  {
-    if($('#widget-check-'+widgetName).hasClass('widget-check')) {
-      $("#widget-box-"+widgetName).remove();
+  var setDragBox = function(element)  {
+    var widgetName = $(element).data('name');
+    if($(element).parent().hasClass('active')) {
+      $("#grid-container").append("<div id='widget-box-"+widgetName+
+        "' class='widget-box widget-box-"+widgetName+
+        "' >"+widgetName+"</div>");
+      $("#widget-box-"+widgetName).draggable(draggableConfig);
     } else {
-      $("#grid-container").append("<div id='widget-box-"+widgetName+"' class='widget-box widget-box-"+widgetName+"' >"+widgetName+"</div>");
-      $("#widget-box-"+widgetName).draggable({ snap: ".grid-box", snapMode: "inner", snapTolerance: 103, containment: "#grid-container", cursor: "pointer" });
+      $("#widget-box-"+widgetName).remove();
     }
   };
 
@@ -44,7 +49,7 @@ $(document).ready(function(){
   };
 
   $('.widget-box').each(function(){
-    $(this).draggable({ snap: ".grid-box", snapMode: "inner", snapTolerance: 103, containment: "#grid-container", cursor: "pointer" });
+    $(this).draggable(draggableConfig);
 
     var location = {
       row: $(this).data('location-row'),
