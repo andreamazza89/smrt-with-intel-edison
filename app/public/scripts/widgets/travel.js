37f3cb245015b95857708eed6a236835eda8e148
Van Le <singularity@vannio.co.uk>
2016-08-01 17:09:10 +0100

    Refactor tfl widget to do mostly server-side rendering

--- app/public/scripts/widgets/travel.js
+++ app/public/scripts/widgets/travel.js
@@ -1,66 +1,46 @@
-// TFL API
-var api = 'https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status';
-var container = $('#tfl-widget');
+;(function(exports){
+  // TFL API
+  var api = 'https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status';
+  var lines = [];
 
-// Pick tube lines of interest
-var lines = [
-  'bakerloo',
-  'central',
-	'circle',
-  'district',
-  'dlr',
-	'hammersmith-city',
-  'jubilee',
-	'northern',
-  'overground',
-	'piccadilly',
-  'tramlink',
-	'victoria',
-  'waterloo-city'
-];
+  $('.tube-status').each(function(){
+    lines.push($(this).data('line'));
+  });
 
-(function(exports){
+	function ajaxCall(){
+		'use strict';
 
-  	function ajaxCall(){
-  		'use strict';
+  	$.ajax({
+	    url: api,
+	    success: function(data) {
+	    	this.printStatus(data);
+	    },
+	    cache: false,
+	    printStatus: function(data) {
+	    	// Loop through set of data for each tube line..
+	    	for (var i = 0; i < data.length; i++){
+	    		// and check if the line matches one of the lines of interest..
+	    		if (lines.indexOf(data[i].id) > -1){
+            var tubeStatus = data[i].lineStatuses[0].statusSeverityDescription;
+            var selector = $("span[data-line='" + data[i].id + "']");
+            selector.text(
+              tubeStatus.charAt(0).toUpperCase() + tubeStatus.slice(1).toLowerCase()
+            );
+          }
+		    }
+	    }
+	  });
+	}
 
-    	$.ajax({
-  	    url: api,
-  	    success: function(data) {
-  	    	this.printStatus(data);
-  	    },
-  	    cache: false,
-  	    printStatus: function(data) {
-  	    	// Clear the container for updates
-  	    	container.empty();
+  function init() {
+  	'use strict';
 
-  	    	// Loop through set of data for each tube line..
-  	    	for (var i = 0; i < data.length; i++){
+  	ajaxCall();
 
-  					var tubeStatus = data[i].lineStatuses[0].statusSeverityDescription;
+  	// Reload info every 5 mins:
+  	setInterval(ajaxCall, 300000);
+  }
 
-  	    		// and check if the line matches one of the lines of interest..
-  	    		if (lines.indexOf(data[i].id) > -1){
-
-  	    			// if so, print the line status:
-  						container.append(
-  							'<li class="flex-container"><span class="tube-status">' + tubeStatus.charAt(0).toUpperCase() + tubeStatus.slice(1).toLowerCase() + ' on the </span><span class="tube-title tube-' + data[i].id + '">' + data[i].name + ' line</span></li>'
-  						);
-  		    	}
-  	    	}
-  	    }
-  	  });
-  	}
-
-    function init() {
-    	'use strict';
-
-    	ajaxCall();
-
-    	// Reload info every 5 mins:
-    	setInterval(ajaxCall, 300000);
-    }
-
-    exports.init = init;
+  exports.init = init;
 
 })(this);
