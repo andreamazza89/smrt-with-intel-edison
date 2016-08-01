cf7e1875d38d7a35a52354d8c33be144186a7acf
Van Le <singularity@vannio.co.uk>
2016-08-02 00:57:39 +0100

    More general tidying

--- app/server.js
+++ app/server.js
@@ -1,30 +1,29 @@
-var fs = require('fs');
-var path = require('path');
-var bodyParser = require('body-parser');
-var browserify = require('browserify-middleware');
-var express = require('express');
-var app = express();
-var nunjucks = require('nunjucks');
-var sass = require('node-sass-middleware');
+var fs = require('fs'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    browserify = require('browserify-middleware'),
+    express = require('express'),
+    app = express(),
+    nunjucks = require('nunjucks'),
+    sass = require('node-sass-middleware');
 
 var WIDGETS_FILE = __dirname + (process.env.widget_path || '/widgets') +  '.json';
 var browserSync = require('browser-sync').create();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'njk');
 
 nunjucks.configure(__dirname + '/views', {
-    watch: true,
-    express: app
-  });
+  watch: true,
+  express: app
+});
 
 app.use(sass({
   src: __dirname + '/sass',
   dest:__dirname + '/public',
   outputStyle: 'compressed'
 }));
-
-app.set('view engine', 'njk');
 
 app.use(express.static('./app/public'));
 app.get('/scripts/mirror-bundle.js', browserify(__dirname + '/public/scripts/mirror.js'));
@@ -49,7 +48,6 @@ app.get('/mirror', function(req, res){
     res.render('mirror', { widgetData: JSON.parse(data).widgets });
   });
 });
-
 
 app.get('/api/widgets', function(req, res) {
   fs.readFile(WIDGETS_FILE, function(err, data) {
