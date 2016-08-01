c8d44bb0ac08f2e2906c766dab7be16c03ba9309
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-08-01 10:00:09 +0100

    grid functions extracted to separate file

---
+++ app/public/scripts/grid-functions.js
@@ -0,0 +1,67 @@
+;(function(exports){
+
+  var positionKey = { 0: 0, 205: 1, 410: 2 };
+  var positionKey2 = { 0: 0, 1: 205, 2: 410 };
+  var draggableConfig = { snap: ".grid-box",
+                          snapMode: "inner",
+                          snapTolerance: 103,
+                          containment: "#grid-container",
+                          cursor: "pointer" };
+
+  var setDragBox = function(element, location)  {
+    var widgetName = $(element).data('name');
+    if($(element).parent().hasClass('active')) {
+      $("#grid-container").append("<div id='widget-box-"+widgetName+
+        "' class='widget-box widget-box-"+widgetName+
+        "' style='top:"+positionKey2[location.row]+"px; "+
+        " left:"+positionKey2[location.column]+"px"+
+        "' data-name='"+widgetName+"'>"+widgetName+"</div>");
+      $("#widget-box-"+widgetName).draggable(draggableConfig);
+    } else {
+      $("#widget-box-"+widgetName).remove();
+    }
+  };
+
+  var gridPosition = function(location) {
+    //defaulting to width/height of 1 for now
+    return {  row: positionKey[location.top],
+              column: positionKey[location.left],
+              height: 1,
+              width: 1 };
+  };
+
+  var getFreeLocation = function()  {
+    var positions = { 0: [], 1: [], 2:[] };
+    $('.widget-box').each(function(){
+      positions[positionKey[$(this).position().top]].push(positionKey[$(this).position().left]);
+    });
+    for(var i = 0; i < 3; i++) {
+      for(var j = 0; j < 3; j++) {
+        if(!positions[i].includes(j)) {
+          return {
+            row: i,
+            column: j,
+            height: 1,
+            width: 1
+          };
+        }
+      }
+    }
+  };
+
+  var spaceFree = function(widget)  {
+    var canMove = true;
+    $('.widget-box').each(function(){
+      if(($(this).data('name') !== widget.data('name')) &&
+         (widget.position().top === $(this).position().top) &&
+         (widget.position().left === $(this).position().left)) {
+        canMove = false;
+      }
+    });
+    return canMove;
+  };
+  exports.setDragBox = setDragBox;
+  exports.gridPosition = gridPosition;
+  exports.getFreeLocation = getFreeLocation;
+  exports.spaceFree = spaceFree;
+})(this);
--- app/public/scripts/interface.js
+++ app/public/scripts/interface.js
@@ -1,15 +1,14 @@
+var positionKey = require('./grid-functions').positionKey;
+var positionKey2 = require('./grid-functions').positionKey2;
+var draggableConfig = require('./grid-functions').draggableConfig;
+
 var getJSON = require('./json').getJSON;
 var setJSON = require('./json').setJSON;
 var widgetJSON = require('./framework').widgetJSON;
-
-var positionKey = { 0: 0, 205: 1, 410: 2 };
-var positionKey2 = { 0: 0, 1: 205, 2: 410 };
-
-var draggableConfig = { snap: ".grid-box",
-                        snapMode: "inner",
-                        snapTolerance: 103,
-                        containment: "#grid-container",
-                        cursor: "pointer" };
+var setDragBox = require('./grid-functions').setDragBox;
+var gridPosition = require('./grid-functions').gridPosition;
+var getFreeLocation = require('./grid-functions').getFreeLocation;
+var spaceFree = require('./grid-functions').spaceFree;
 
 $(document).ready(function(){
 
@@ -42,59 +41,6 @@ $(document).ready(function(){
       activeWidget.css('left', activeLocation.left);
     }
   });
-
-  var setDragBox = function(element, location)  {
-    var widgetName = $(element).data('name');
-    if($(element).parent().hasClass('active')) {
-      $("#grid-container").append("<div id='widget-box-"+widgetName+
-        "' class='widget-box widget-box-"+widgetName+
-        "' style='top:"+positionKey2[location.row]+"px; "+
-        " left:"+positionKey2[location.column]+"px"+
-        "' data-name='"+widgetName+"'>"+widgetName+"</div>");
-      $("#widget-box-"+widgetName).draggable(draggableConfig);
-    } else {
-      $("#widget-box-"+widgetName).remove();
-    }
-  };
-
-  var gridPosition = function(location) {
-    //defaulting to width/height of 1 for now
-    return {  row: positionKey[location.top],
-              column: positionKey[location.left],
-              height: 1,
-              width: 1 };
-  };
-
-  var getFreeLocation = function()  {
-    var positions = { 0: [], 1: [], 2:[] };
-    $('.widget-box').each(function(){
-      positions[positionKey[$(this).position().top]].push(positionKey[$(this).position().left]);
-    });
-    for(var i = 0; i < 3; i++) {
-      for(var j = 0; j < 3; j++) {
-        if(!positions[i].includes(j)) {
-          return {
-            row: i,
-            column: j,
-            height: 1,
-            width: 1
-          };
-        }
-      }
-    }
-  };
-
-  var spaceFree = function(widget)  {
-    var canMove = true;
-    $('.widget-box').each(function(){
-      if(($(this).data('name') !== widget.data('name')) &&
-         (widget.position().top === $(this).position().top) &&
-         (widget.position().left === $(this).position().left)) {
-        canMove = false;
-      }
-    });
-    return canMove;
-  };
 
   $('.widget-box').each(function(){
     $(this).draggable(draggableConfig);
