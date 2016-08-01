02638656cb9d4bfc2329f09d061c17fab7277a8d
Luke <lukeaveil@yahoo.com>
2016-08-01 12:12:35 +0100

    adds radio settings to widgets json

--- app/widgets/clock.js
+++ app/widgets/clock.js
@@ -2,7 +2,12 @@
 
   var displayDateTime = require('./clock-view').displayDateTime;
   var fetchCurrentDateTime = require('./clock-data').fetchCurrentDateTime;
-  
+
+  function startRadio() {
+    var isRadioEnabled = $("#clock-widget .inner").data('radio-enabled');
+    if(isRadioEnabled) {console.log("it works");}
+  }
+
   function init() {
     'use strict';
 
@@ -10,6 +15,7 @@
     setInterval(function() {
       var dateTime = fetchCurrentDateTime();
       displayDateTime(dateTime);
+      startRadio();
     }, 1000);
   }
 
