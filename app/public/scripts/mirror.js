51ed2f3f6cf1031fb578f27efda575023f0c6d0c
Luke <lukeaveil@yahoo.com>
2016-08-01 16:38:18 +0100

    gesture control for radio

--- app/public/scripts/mirror.js
+++ app/public/scripts/mirror.js
@@ -11,27 +11,28 @@ var lastGestureTime = Date.now();
 $(document).ready(function(){
   var controller  = new Leap.Controller({enableGestures: true});
   controller.on('deviceFrame', function(frame) {
-    for(var i = 0; i < frame.gestures.length; i++){
-      var gesture = frame.gestures[i];
-      var type = gesture.type;
+    var gesture = frame.gestures[0];
+    var type = gesture.type;
 
-      switch(type){
-        case "swipe":
-          if (gesture.state == "stop") {}
-          break;
-
-        case "keyTap":
-          if (gesture.state == "stop" && Date.now() >= lastGestureTime + 500) {
-            $('.main-content').toggleClass('hidden');
-            lastGestureTime = Date.now();
-          }
-          break;
-
-          case "screenTap":
-          if (gesture.state == "stop") {}
-          break;
+    switch(type){
+      case "swipe":
+        if (gesture.state == "stop") {
+          $('#audio-player').trigger('pause');
+          radioIsNotLoaded = true;
         }
-      }
+        break;
+
+      case "keyTap":
+        if (gesture.state == "stop" && Date.now() >= lastGestureTime + 500) {
+          $('.main-content').toggleClass('hidden');
+          lastGestureTime = Date.now();
+        }
+        break;
+
+        case "screenTap":
+        if (gesture.state == "stop") {}
+        break;
+    }
   });
 
   controller.connect();
