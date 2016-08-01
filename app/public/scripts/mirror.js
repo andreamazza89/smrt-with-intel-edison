efd492d246fbf853d5f605a06c10a04e4bca0fb5
Van Le <singularity@vannio.co.uk>
2016-08-01 15:41:31 +0100

    Refactor mirror/control-panel css and js file names

--- app/public/scripts/mirror.js
+++ app/public/scripts/mirror.js
@@ -6,42 +6,29 @@ weather.init();
 travel.init();
 clock.init();
 
+var lastGestureTime = Date.now();
+
 $(document).ready(function(){
-
   var controller  = new Leap.Controller({enableGestures: true});
-
-  var widgets;
-
   controller.on('deviceFrame', function(frame) {
-    var body;
-
     for(var i = 0; i < frame.gestures.length; i++){
       var gesture = frame.gestures[i];
-      var type    = gesture.type;
+      var type = gesture.type;
 
-      switch( type ){
-
+      switch(type){
         case "swipe":
-          if (gesture.state == "stop") {
-
-          }
+          if (gesture.state == "stop") {}
           break;
 
         case "keyTap":
-          if (gesture.state == "stop") {
-            // if( console.log(Date.now() - lastGestureTime)  > 3){
-              body = document.getElementById('body');
-              body.style.color = (body.style.color !== 'black' ? 'black' : 'white');
-            //   lastGestureTime = Date.now() ;
-            // }
-
+          if (gesture.state == "stop" && Date.now() >= lastGestureTime + 500) {
+            $('.main-content').toggleClass('hidden');
+            lastGestureTime = Date.now();
           }
           break;
 
           case "screenTap":
-          if (gesture.state == "stop") {
-
-          }
+          if (gesture.state == "stop") {}
           break;
         }
       }
@@ -57,30 +44,13 @@ $(document).ready(function(){
     }
   }
 
-  widgets = document.querySelectorAll('#weather-widget, #tfl-widget, #clock-widget');
-
-  for (var i = 0; i < widgets.length; i++) {
-    bind(widgets[i], 'mouseenter', 	function() { // jshint ignore:line
-      this.style.background = '#171717';
-      this.style.transition = '0.5s';
-      var div = $(this);
-      div.stop(true, true).animate({
-          margin: -10,
-          width: "+=20",
-          height: "+=20"
-      }, 'fast');
+  $('.mirror-widget').each(function(){
+    bind(this, 'mouseenter', function(){
+      $(this).addClass('active');
     });
 
-    bind(widgets[i], 'mouseleave', 	function() { // jshint ignore:line
-      this.style.background = 'none';
-      this.style.transition = '0.5s';
-      var div = $(this);
-      div.stop(true, true).animate({
-          margin: 0,
-          width: "-=20",
-          height: "-=20"
-      }, 'fast');
+    bind(this, 'mouseleave', function(){
+      $(this).removeClass('active');
     });
-  }
-
+  });
 });
