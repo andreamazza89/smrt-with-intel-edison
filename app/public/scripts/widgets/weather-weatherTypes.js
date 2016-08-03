4f03486a501ad52f58fe467de08fa3d27cd09e46
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-08-03 12:11:23 +0100

    Refactors weather script to import the icons

---
+++ app/public/scripts/widgets/weather-weatherTypes.js
@@ -0,0 +1,37 @@
+// N = Night, D = Day
+var weatherType = {
+	'NA':	'Not available',
+	'0': 'Clear night',
+	'1': 'Sunny day',
+	'2': 'Partly cloudy (N)',
+	'3': 'Partly cloudy (D)',
+	'4': 'Not used',
+	'5': 'Mist',
+	'6': 'Fog',
+	'7': 'Cloudy',
+	'8': 'Overcast',
+	'9': 'Light rain shower (N)',
+	'10': 'Light rain shower (D)',
+	'11': 'Drizzle',
+	'12': 'Light rain',
+	'13': 'Heavy rain shower (N)',
+	'14': 'Heavy rain shower (D)',
+	'15': 'Heavy rain',
+	'16': 'Sleet shower (N)',
+	'17': 'Sleet shower (D)',
+	'18': 'Sleet',
+	'19': 'Hail shower (N)',
+	'20': 'Hail shower (D)',
+	'21': 'Hail',
+	'22': 'Light snow shower (N)',
+	'23': 'Light snow shower (D)',
+	'24': 'Light snow',
+	'25': 'Heavy snow shower (N)',
+	'26': 'Heavy snow shower (D)',
+	'27': 'Heavy snow',
+	'28': 'Thunder shower (N)',
+	'29': 'Thunder shower (D)',
+	'30': 'Thunder'
+};
+
+module.exports = weatherType;
--- app/public/scripts/widgets/weather.js
+++ app/public/scripts/widgets/weather.js
@@ -1,45 +1,9 @@
 // Metoffice API
 var icons = require('./icons').icons;
+var weatherType = require('./weather-weatherTypes'); 
 var api = 'http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/352409?res=3hourly&key=b821346b-d530-4054-a2e6-2fee05048742';
 var textContainer = $('#weather-widget .text');
 var iconContainer = $('#weather-widget .icon');
-
-// N = Night, D = Day
-var weatherType = {
-	'NA':	'Not available',
-	'0': 'Clear night',
-	'1': 'Sunny day',
-	'2': 'Partly cloudy (N)',
-	'3': 'Partly cloudy (D)',
-	'4': 'Not used',
-	'5': 'Mist',
-	'6': 'Fog',
-	'7': 'Cloudy',
-	'8': 'Overcast',
-	'9': 'Light rain shower (N)',
-	'10': 'Light rain shower (D)',
-	'11': 'Drizzle',
-	'12': 'Light rain',
-	'13': 'Heavy rain shower (N)',
-	'14': 'Heavy rain shower (D)',
-	'15': 'Heavy rain',
-	'16': 'Sleet shower (N)',
-	'17': 'Sleet shower (D)',
-	'18': 'Sleet',
-	'19': 'Hail shower (N)',
-	'20': 'Hail shower (D)',
-	'21': 'Hail',
-	'22': 'Light snow shower (N)',
-	'23': 'Light snow shower (D)',
-	'24': 'Light snow',
-	'25': 'Heavy snow shower (N)',
-	'26': 'Heavy snow shower (D)',
-	'27': 'Heavy snow',
-	'28': 'Thunder shower (N)',
-	'29': 'Thunder shower (D)',
-	'30': 'Thunder'
-};
-
 
 (function(exports){
   $.support.cors = true;
