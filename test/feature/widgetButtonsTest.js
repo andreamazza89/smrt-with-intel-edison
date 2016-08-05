43bffc9394bc8d858a98cb96367a982a87cf80e0
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-08-05 14:01:17 +0100

    config engine feature tests added

--- test/feature/widgetButtonsTest.js
+++ test/feature/widgetButtonsTest.js
@@ -1,28 +1,40 @@
-// var app = require('../../../app/server');
-// var Browser = require('zombie');
-// var expect = require('chai').expect;
-//
-// describe('Widget buttons', function(){
-//   var browser;
-//
-//    before(function(){
-//      browser = new Browser({ site: 'http://localhost:3000' });
-//    });
-//
-//    before(function(done){
-//      browser.visit('/', function(){
-//        done();
-//      });
-//    });
-//
-//   it('should create a box when widget enabled', function(){
-//     browser.pressButton("#widget-check-weather");
-//     expect(browser.html()).to.contain('id="widget-box-weather"');
-//   });
-//
-//   it('should remove a box when widget disabled', function(){
-//     browser.pressButton("#widget-check-weather");
-//     expect(browser.html()).to.not.contain('id="widget-box-weather"');
-//   });
-//
-// });
+require('../../app/server');
+var expect = require('chai').expect;
+
+describe('News Feed Widget', function(){
+  before(function(){
+    process.env.widget_path = '/../test/widget-configs/news-active';
+    browser
+      .url('/');
+  });
+
+  it('should be able to drag and drop widgets', function(){
+    browser.dragAndDrop('#widget-box-rss', '#grid-3');
+    browser.url('/mirror');
+    expect(browser.element('#rss-widget').getAttribute('class')).to.contain('row-1');
+    expect(browser.element('#rss-widget').getAttribute('class')).to.contain('column-0');
+
+    browser.url('/');
+    browser.dragAndDrop('#widget-box-rss', '#grid-1');
+    browser.url('/mirror');
+    expect(browser.element('#rss-widget').getAttribute('class')).to.contain('row-0');
+    expect(browser.element('#rss-widget').getAttribute('class')).to.contain('column-1');
+  });
+
+  it('should not be able to drag widget onto existing widget', function(){
+    process.env.widget_path = '/../test/widget-configs/multiple-active';
+    browser.url('/');
+    browser.dragAndDrop('#widget-box-weather', '#grid-0');
+    expect(browser.element('#widget-box-weather').getAttribute('style')).to.contain('left: 205px');
+  });
+
+  it('new widget gets populated in next free location', function(){
+    browser.click('#widget-check-rss');
+    browser.url('/mirror');
+    expect(browser.element('#rss-widget').getAttribute('class')).to.contain('row-1');
+    expect(browser.element('#rss-widget').getAttribute('class')).to.contain('column-0');
+    browser.url('/');
+    browser.click('#widget-check-rss');
+  });
+
+});
